1c3061b6e57b0ed3ce990f40640ba18becaa0657
dimnov <dimnov1512@gmail.com>
2023-05-23 18:20:22 +0300

    Add Express to project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 const PORT = 5000;
 
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 app.get('/', (req, res) => {
-  res.send("It's working");
+  res.render('index')
+});
+
+app.get('/create', (req, res) => {
+  res.render('create')
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
+app.get('/details', (req, res) => {
+  res.render('details')
+});
+
+app.get('*', (req, res) => {
+  res.render('404')
 });
 
 app.listen(PORT, () => {
