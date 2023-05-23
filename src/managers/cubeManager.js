fbb74b9ab73cbfe8e45dad56c0facb8f31f2eabe
dimnov <dimnov1512@gmail.com>
2023-05-23 20:58:40 +0300

    Add id generator

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,10 +1,12 @@
+const uniqid = require('uniqid');
+
 const cubes = [];
 
 exports.getAll = () => cubes.slice();
 
 exports.create = (cubeData) => {
   const newCube = {
-    id: cubes.length + 1,
+    id: uniqid(),
     ...cubeData,
   }
 
