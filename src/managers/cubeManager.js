9dee1cd8b0ced4abdb160792d81d7a757d82c086
dimnov <dimnov1512@gmail.com>
2023-05-23 22:30:36 +0300

    Revert "Add cube details page"
    This reverts commit 933c7d8a88efdbfd99414fb6b8a7a887c9dc4d4e.

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -2,7 +2,6 @@ const uniqid = require('uniqid');
 const cubes = [];
 
 exports.getAll = () => cubes.slice();
-exports.getOne = (cubeId) => cubes.find(x => x.id === cubeId);
 
 exports.create = (cubeData) => {
   const newCube = {
