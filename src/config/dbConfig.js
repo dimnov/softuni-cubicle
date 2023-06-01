f07304b6c580d58dd9f91e33096be632e4a5fe81
dimnov <dimnov1512@gmail.com>
2023-06-01 21:13:53 +0300

    Update config

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const uri = 'mongodb://127.0.0.1:27017/cubicle';
 
 async function dbConnect() {
-  await mongoose.connect(uri);
+    await mongoose.connect(uri);
 }
 
 module.exports = dbConnect;
