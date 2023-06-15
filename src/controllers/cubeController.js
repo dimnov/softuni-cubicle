eb8c52997f78e7a875363190a870cb3964ad4029
dimnov <dimnov1512@gmail.com>
2023-06-15 17:20:27 +0300

    Update cube

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -60,10 +60,27 @@ router.get('/:cubeId/delete', async (req, res) => {
     res.render('cube/delete', { cube });
 });
 
+router.post('/:cubeId/delete', async (req, res) => {
+    await cubeManager.delete(req.params.cubeId);
+
+    res.redirect('/');
+});
+
+router.get('/:cubeId/edit', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
 
 
-router.get('/:cubeId/edit', (req, res) => {
-    res.render('cube/edit');
+
+
+    res.render('cube/edit', { cube });
+});
+
+router.post('/:cubeId/edit', async (req, res) => {
+    const { name, description, imageUrl, difficultyLevel } = req.body;
+
+    await cubeManager.update(req.params.cubeId, { name, description, imageUrl, difficultyLevel });
+
+    res.redirect(`/cubes/${req.params.cubeId}/details`);
 });
 
 module.exports = router;
